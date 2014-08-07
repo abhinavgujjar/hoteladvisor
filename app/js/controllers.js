5a92a2b8514a804b3d0fe13057e754340de48729
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-08-07 16:39:21 +0530

    services

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,63 +3,18 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-	.controller('mainController', ['$scope',
-		function($scope) {
+	.controller('mainController', ['$scope', 'defaults', 'hotelsProvider', 'votingService',
+		function($scope, defaults, hotelsProvider, vService) {
 
 			$scope.message = "Good Morning";
 
-			$scope.limit = 15000;
+			$scope.limit = defaults.rateLimit;
+			$scope.descLimit = defaults.descLimit;
 
-			$scope.highlight_class = 'highlight_green';
+			$scope.upvote = vService.upVote;
+			$scope.downvote = vService.downVote;
 
-			$scope.upvote = function(hotel) {
-				if (!hotel.rating) {
-					hotel.rating = 0;
-				}
-
-				hotel.rating++;
-			}
-
-			$scope.downvote = function(hotel) {
-				if (!hotel.rating) {
-					hotel.rating = 0;
-				}
-				hotel.rating--;
-			}
-
-
-
-			$scope.hotels = [{
-				name: 'Mariott',
-				location: {
-					area: 'Whitefield',
-					city: 'Bangalore'
-				},
-				price: 13000,
-				img: 'Ds7MGeY.jpg',
-				description: 'Triumphantly much unlike and whooped scratched intense a well sensibly below that much pugnaciously morbid that caterpillar over that soberly seal and labrador far iguanodon far amongst rhinoceros raccoon that haughty won much.',
-				area : 50000
-			}, {
-				name: 'Taj West End',
-				location: {
-					area: 'Race Course Road',
-					city: 'Bangalore'
-				},
-				price: 17000,
-				img: 'BQ1bQ6T.jpg',
-				description: 'This is a hotel',
-				area : 120000
-			}, {
-				name: 'Leela Palace',
-				location: {
-					area: 'Old Airport Road',
-					city: 'Bangalore'
-				},
-				price: 30000,
-				img: 'O3V6rIK.jpg',
-				description: 'Or oh active bewitching lent a yikes well ouch much jeez the quail abrasive reserved since classic a then matter-of-fact shuddered.',
-				area : 36000
-			}]
+			$scope.hotels = hotelsProvider.getHotels();
 
 		}
 	])
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,81 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+value('version', '0.1').
+value('rateLimit', 15000).
+value('defaults', {
+	rateLimit: 15000,
+	descLimit: 50
+}).
+factory('votingService', function() {
+	return {
+		upVote: function(hotel) {
+			if (!hotel.rating) {
+				hotel.rating = 0;
+			}
+
+			hotel.rating++;
+		},
+		downVote: function(hotel) {
+			if (!hotel.rating) {
+				hotel.rating = 0;
+			}
+
+			if (hotel.rating > 0) {
+				hotel.rating--;
+			}
+		}
+	}
+}).
+factory('hotelsProvider', function() {
+
+	var hotels = [{
+		name: 'Mariott',
+		location: {
+			area: 'Whitefield',
+			city: 'Bangalore'
+		},
+		price: 13000,
+		img: 'Ds7MGeY.jpg',
+		description: 'Triumphantly much unlike and whooped scratched intense a well sensibly below that much pugnaciously morbid that caterpillar over that soberly seal and labrador far iguanodon far amongst rhinoceros raccoon that haughty won much.',
+		area: 50000
+	}, {
+		name: 'Taj West End',
+		location: {
+			area: 'Race Course Road',
+			city: 'Bangalore'
+		},
+		price: 17000,
+		img: 'BQ1bQ6T.jpg',
+		description: 'This is a hotel',
+		area: 120000
+	}, {
+		name: 'Leela Palace',
+		location: {
+			area: 'Old Airport Road',
+			city: 'Bangalore'
+		},
+		price: 30000,
+		img: 'O3V6rIK.jpg',
+		description: 'Or oh active bewitching lent a yikes well ouch much jeez the quail abrasive reserved since classic a then matter-of-fact shuddered.',
+		area: 36000
+	}, {
+		name: 'Hilton',
+		location: {
+			area: 'Lavelle Road',
+			city: 'Bangalore'
+		},
+		price: 30000,
+		img: 'FS46AwM.jpg',
+		description: 'Or oh active bewitching lent a yikes well ouch much jeez the quail abrasive reserved since classic a then matter-of-fact shuddered.',
+		area: 45000
+	}];
+
+
+	return {
+		getHotels: function() {
+			return hotels;
+		}
+	};
+
+});
