e3dc63ad2822878ca5dba5a55a7c058c3975a2fd
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-08-07 15:25:47 +0530

    bootstrap added in

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,61 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+	.controller('mainController', ['$scope',
+		function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+			$scope.message = "Good Morning";
 
-  }]);
+			$scope.limit = 15000;
+
+			$scope.highlight_class = 'highlight_green';
+
+			$scope.upvote = function(hotel) {
+				if (!hotel.rating) {
+					hotel.rating = 0;
+				}
+
+				hotel.rating++;
+			}
+
+			$scope.downvote = function(hotel) {
+				if (!hotel.rating) {
+					hotel.rating = 0;
+				}
+				hotel.rating--;
+			}
+
+
+			$scope.hotels = [{
+				name: 'Mariott',
+				location: {
+					area: 'Whitefield',
+					city: 'Bangalore'
+				},
+				price: 13000,
+				img: 'Ds7MGeY.jpg'
+			}, {
+				name: 'Taj West End',
+				location: {
+					area: 'Race Course Road',
+					city: 'Bangalore'
+				},
+				price: 17000,
+				img: 'BQ1bQ6T.jpg'
+			}, {
+				name: 'Leela Palace',
+				location: {
+					area: 'Old Airport Road',
+					city: 'Bangalore'
+				},
+				price: 30000,
+				img: 'O3V6rIK.jpg'
+			}]
+
+		}
+	])
+	.controller('MyCtrl2', ['$scope',
+		function($scope) {
+
+		}
+	]);
