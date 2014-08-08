8025ff0a17aa56bf65f9389407ba71d1410b8f18
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-08-08 09:42:56 +0530

    added route

--- app/js/app.js
+++ app/js/app.js
@@ -9,9 +9,9 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/listing', {templateUrl: 'partials/listing.html'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html'});
+  $routeProvider.otherwise({redirectTo: '/login'});
 }]);
 
 angular.module('myApp').
